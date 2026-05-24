9993439555a4b456ebae482827e71874c2832980
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2026-05-24 00:46:20 -0700

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+                
+                
+                
+                
+                
+        res = []
+        print(count)
+        for k, v in count.items():
+            if v > len(nums) // 3:
+                res.append(k)
+
+        return res
+
+        
