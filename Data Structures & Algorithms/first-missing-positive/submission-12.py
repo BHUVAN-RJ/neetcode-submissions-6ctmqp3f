b556d0a570df6dadd76440bc293aa791d138e5b8
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2026-05-27 21:42:06 -0700

    Add: first-missing-positive - submission-12

---
+++ Data Structures & Algorithms/first-missing-positive/submission-12.py
@@ -0,0 +1,11 @@
+# simpler uses hash set to store the values and give solution - easier
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        for i in range(1, len(nums) + 1):
+            if i not in nums:
+                return i
+        return len(nums ) + 1
+
+        
