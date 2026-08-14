339e23ce3925f57bf4d0a287b0a4fd53dbfa8208
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2026-08-14 16:13:30 -0700

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,22 @@
+'''
+simple dp problem - 
+[2, 9, 10, 12, 16]
+prev = 9
+prev_prev = 2
+so I need this is max(prev_sum, cur+prev_prev)
+
+'''
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <=2:
+            return max(nums)
+        
+        dp = [nums[0], max(nums[0], nums[1])]
+
+        for i in range(2, len(nums)):
+            dp.append(max(dp[i - 1], dp[i - 2] + nums[i]))
+        
+        return dp[-1]
+
+        
