ddba3e7f53c96b9772de32eaece5d5317721b272
Bhuvan R J <83565908+BHUVAN-RJ@users.noreply.github.com>
2026-09-15 11:59:39 -0700

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        cur = []
+        wordDict = set(wordDict)
+
+        def backtrack(i):
+            if i == len(s):
+                res.append(" ".join(cur))
+                return
+            
+            for j in range(len(s)):
+                w = s[i:j+1]
+                if w in wordDict:
+                    cur.append(w)
+                    backtrack(j + 1)
+                    cur.pop()
+            
+        backtrack(0)
+        return res
+
+
+        
